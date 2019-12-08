88919cbe4f9f836b407cc02bb2299a8821bf427e
738866 <738866@unizar.es>
2019-12-08 12:31:33 +0100

    isAccesible improved and timeout added

--- src/main/java/urlshortener/web/UrlShortenerController.java
+++ src/main/java/urlshortener/web/UrlShortenerController.java
@@ -20,10 +20,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
-
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -51,19 +52,27 @@ public class UrlShortenerController {
     }
 
     private boolean isAccesible(String url_s) {
-        int responseCode = 400;
+        boolean ret = false;
         try {
-            URL url = new URL(url_s);
-            HttpURLConnection huc = (HttpURLConnection) url.openConnection();
-            huc.setRequestMethod("HEAD");
-            responseCode = huc.getResponseCode();
-        } catch (MalformedURLException e1) {
-            //e1.printStackTrace();
-        } catch (IOException e) {
-            //e.printStackTrace();
+            ret = InetAddress.getByName(new URL(url_s).getHost()).isReachable(1000);
+            if (!ret) {
+                int responseCode = 400;
+                URL url = new URL(url_s);
+                HttpURLConnection huc = (HttpURLConnection) url.openConnection();
+                huc.setRequestMethod("HEAD");
+                huc.setConnectTimeout(1000);
+                huc.setReadTimeout(1000);
+                responseCode = huc.getResponseCode();
+                ret = responseCode == HttpURLConnection.HTTP_OK;
+            }
+        } catch (UnknownHostException e2) {
+            e2.printStackTrace();
+            ret = false;
+        } catch (IOException e2) {
+            e2.printStackTrace();
+            ret = false;
         }
-
-        return responseCode == HttpURLConnection.HTTP_OK;
+        return ret;
     }
 
 @RequestMapping(value = {"/{id:(?!link|index).*}","/{id:(?!link|index|webjars|js|bootstrap)[a-z0-9]*}/**"}, method = RequestMethod.GET)
