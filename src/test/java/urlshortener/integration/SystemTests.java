54053ca62dae44502f6f759015dedede0ad02078
Victor M. Batlle <736478@unizar.es>
2020-01-10 18:32:52 +0100

    Fix: test due to change in final URI

--- src/test/java/urlshortener/integration/SystemTests.java
+++ src/test/java/urlshortener/integration/SystemTests.java
@@ -93,12 +93,12 @@ public class SystemTests {
         ResponseEntity<String> entity = postLink("http://goo.gl/fb/gyBkwR/");
 
         assertThat(entity.getStatusCode(), is(HttpStatus.CREATED));
-        assertThat(entity.getHeaders().getLocation(), is(new URI("http://localhost:" + this.port + "/9a2911b4")));
+        assertThat(entity.getHeaders().getLocation(), is(new URI("http://localhost:" + this.port + "/f4f5f3a2")));
         assertThat(entity.getHeaders().getContentType(), is(new MediaType("application", "json", StandardCharsets.UTF_8)));
         ReadContext rc = JsonPath.parse(entity.getBody());
-        assertThat(rc.read("$.hash"), is("9a2911b4"));
-        assertThat(rc.read("$.uri"), is("http://localhost:" + this.port + "/9a2911b4"));
-        assertThat(rc.read("$.target"), is("https://www.mkyong.com/mongodb/mongodb-remove-a-field-from-array-documents/"));
+        assertThat(rc.read("$.hash"), is("f4f5f3a2"));
+        assertThat(rc.read("$.uri"), is("http://localhost:" + this.port + "/f4f5f3a2"));
+        assertThat(rc.read("$.target"), is("https://mkyong.com/mongodb/mongodb-remove-a-field-from-array-documents/"));
     }
 
     @Test
